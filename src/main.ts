0f8dd934a4389964d1bb70bd0f25088c38b00d7b
liuchang <liuchang@fit2cloud.com>
2023-12-27 15:28:13 +0800

    feat(theme setting):init

---
+++ src/main.ts
@@ -0,0 +1,35 @@
+import "./css/core.css";
+
+
+const toggleMenu = (event:Event) => {
+  const menu = document.querySelector('.menu__list');
+  menu?.classList.toggle('menu__list--active');
+  menu?.classList.toggle('menu__list--transition');
+  const target = event.currentTarget as Element
+  target.classList.toggle('menu__btn--active');
+  target.setAttribute(
+    'aria-expanded',
+    target.getAttribute('aria-expanded') === 'true' ? 'false' : 'true'
+  );
+}
+
+const removeMenuTransition = (event: Event) => {
+  const target = event.target as Element
+  target.classList.remove('menu__list--transition');
+}
+
+const init = () => {
+  const menuBtn = document.querySelector('.menu__btn');
+  const menu = document.querySelector('.menu__list');
+  menuBtn?.addEventListener('click', toggleMenu, false);
+  menu?.addEventListener('transitionend', removeMenuTransition, false);
+
+  // 出发搜索按钮
+  const widget = document.querySelector(".widget-search__field");
+  widget?.addEventListener('focus', function () {
+    SearchWidget.open()
+  })
+}
+
+window.addEventListener("DOMContentLoaded", init);
+
