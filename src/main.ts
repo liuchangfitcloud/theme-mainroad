4c86804c91d07c26ab93e558e33cd26f9623441e
liuchang <liuchang@fit2cloud.com>
2024-01-03 10:18:46 +0800

    feat(theme setting):add photos page

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,26 @@ const init = () => {
   const menu = document.querySelector('.menu__list');
   menuBtn?.addEventListener('click', toggleMenu, false);
   menu?.addEventListener('transitionend', removeMenuTransition, false);
+
+  const images = document.querySelectorAll(".photos_content_img > img");
+  if (images) {
+    images.forEach((img:Element) => {
+      img.addEventListener("click", function (event: Event) { 
+        const currentImg = event.target as HTMLElement;
+        const imageSrc = currentImg.getAttribute("src");
+        const imageBox = document.querySelector(".image-box");
+        if (imageSrc) {
+          imageBox?.querySelector('img')?.setAttribute('src', imageSrc);
+          imageBox?.parentElement?.classList.toggle('show')
+        }
+      })
+    })
+  }
+
+  document.querySelector(".close-btn")?.addEventListener("click", function (event: Event) {
+    const closeBtn: HTMLElement = event.currentTarget as HTMLElement;
+    closeBtn.parentElement?.classList.toggle('show')
+  })
 }
 
 window.addEventListener("DOMContentLoaded", init);
